813306d7353fa53ee6b0917e6268bbd2abf4c656
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2024-09-27 11:49:48 -0500

    Final Commit

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FormattedMessage } from 'react-intl';
 
+
 function Detail({ robot }){
   if (!robot) return null;
 
